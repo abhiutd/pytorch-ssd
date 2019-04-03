c66edd6e52f37b2e2cce529b64e11ab253071918
abhiutd <abhibits2012@gmail.com>
2019-04-03 13:52:34 -0500

    add sample

--- sample.py
+++ sample.py
@@ -4,17 +4,18 @@ from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_
 from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
 from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
 from vision.utils.misc import Timer
+import torch 
 import cv2
 import sys
 
 
-if len(sys.argv) < 5:
-    print('Usage: python run_ssd_example.py <net type>  <model path> <image path>')
+if len(sys.argv) < 2:
+    print('Usage: python run_ssd_example.py <net type>  <model path>')
     sys.exit(0)
 net_type = sys.argv[1]
 model_path = sys.argv[2]
 label_path = sys.argv[3]
-image_path = sys.argv[4]
+#image_path = sys.argv[4]
 
 class_names = [name.strip() for name in open(label_path).readlines()]
 
@@ -46,28 +47,30 @@ elif net_type == 'sq-ssd-lite':
 else:
     predictor = create_vgg_ssd_predictor(net, candidate_size=200)
 
-orig_image = cv2.imread(image_path)
-image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
-### generate trace module
+#orig_image = cv2.imread(image_path)
+#image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+
+### Usual predict
+# boxes, labels, probs = predictor.predict(image, 10, 0.4)
+
+
 image_size = 300
 
 fake_input = torch.rand(1, 3, image_size, image_size)
 traced_script_module = torch.jit.trace(net, fake_input, check_trace=False)
-traced_script_module.save("libtorch/" + net_type + ".pt")
-#boxes, labels, probs = predictor.predict(image, 10, 0.4)
-'''
-for i in range(boxes.size(0)):
-    box = boxes[i, :]
-    cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
-    #label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
-    label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
-    cv2.putText(orig_image, label,
-                (box[0] + 20, box[1] + 40),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,  # font scale
-                (255, 0, 255),
-                2)  # line type
-path = "run_ssd_example_output.jpg"
-cv2.imwrite(path, orig_image)
-print(f"Found {len(probs)} objects. The output image is {path}")
-'''
+traced_script_module.save("test" + ".pt")
+
+# for i in range(boxes.size(0)):
+#     box = boxes[i, :]
+#     cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+#     #label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
+#     label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+#     cv2.putText(orig_image, label,
+#                 (box[0] + 20, box[1] + 40),
+#                 cv2.FONT_HERSHEY_SIMPLEX,
+#                 1,  # font scale
+#                 (255, 0, 255),
+#                 2)  # line type
+# path = "run_ssd_example_output.jpg"
+# cv2.imwrite(path, orig_image)
+# print(f"Found {len(probs)} objects. The output image is {path}")
